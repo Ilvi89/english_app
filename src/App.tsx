ee4985ea55b4df91e1f9392360d336626b6d1a37
Pizik Ilya <ilya.pizik89@gmail.com>
2023-04-16 14:28:12 +0300

    add grommet ui and create first game

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ReactNode, useState} from "react";
+// import logo from './logo.svg';
+// import './App.css';
+import {Box, Button, grommet, Grommet, Header, Page, PageContent, PageHeader, Text} from "grommet";
+import {Moon, Sun} from "grommet-icons";
+import {deepMerge} from "grommet/utils";
+import MakeUpWord from "./games/MakeUpWord";
+
+const theme = deepMerge(grommet, {
+    global: {
+        colors: {
+            brand: '#228BE6',
+        },
+        font: {
+            family: "Roboto",
+            size: "18px",
+            height: "20px",
+        },
+    },
+});
+
+const AppBar = (props: { children: ReactNode }) => (
+    <Header
+        background="brand"
+        pad={{left: "medium", right: "small", vertical: "small"}}
+        elevation="medium"
+        {...props}
+    />
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [dark, setDark] = useState(false);
+
+    return (
+        <Grommet full theme={theme} themeMode={dark ? "dark" : "light"}>
+            <Page>
+                <AppBar>
+                    <Text>ENGLISH APP</Text>
+                    <Button
+                        a11yTitle={dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                        icon={dark ? <Moon/> : <Sun/>}
+                        onClick={() => setDark(!dark)}
+                        tip={{
+                            content: (
+                                <Box
+                                    pad="small"
+                                    round="small"
+                                    background={dark ? "dark-1" : "light-3"}
+                                >
+                                    {dark ? "Switch to Light Mode" : "Switch to Dark Mode"}
+                                </Box>
+                            ),
+                            plain: true,
+                        }}
+                    />
+                </AppBar>
+                <PageContent>
+                    {/*<PageHeader title="Hello world"/>*/}
+                    <MakeUpWord inputType="dnd"/>
+                </PageContent>
+            </Page>
+        </Grommet>
+    );
 }
 
 export default App;
